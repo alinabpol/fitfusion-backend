d0eb0e1b7030464d2d2101d9acb74b992e39ac5a
Alina232323 <alina.polianska@vergesense.com>
2023-05-21 18:07:51 -0700

    added get route for nutrition model

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 
-from flask import Flask
-
+from flask import Flask, jsonify, g
+from flask_cors import CORS
 
 DEBUG = True
 PORT = 8000
 
+import models
+
+from resources.nutrition import nutrition
+
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request"""
+    g.db.close()
+    return response
+
+
+CORS(nutrition, origins=['http://localhost:3000'], supports_credentials=True)
+app.register_blueprint(nutrition, url_prefix='/api/v1/nutrition')
+
+
+
+if __name__ == '__main__':
+    print('tables connected')
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
+
 
 
 
---
+++ models.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+DATABASE = SqliteDatabase('nutrition.sqlite')
+
+
+class Nutrition(Model):
+    name = CharField()
+    description = CharField()
+    time = TimeField()
+
+    class Meta:
+        database = DATABASE
+
+
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Nutrition], safe=True)
+    print("TABLES Created")
+    DATABASE.close()
