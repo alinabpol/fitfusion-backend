f3ecc93f4a9276a22242a7315a39fa65d1da14c2
Alina232323 <alina.polianska@vergesense.com>
2023-06-05 22:06:54 -0700

    removed mongoclient

---
+++ app.py
@@ -0,0 +1,80 @@
+
+import os
+from flask import Flask, g
+from flask_cors import CORS
+import pymongo
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+DEBUG = True
+PORT = 8000
+
+
+
+app = Flask(__name__)
+client = os.getenv("ATLAS_URI")
+
+
+import models
+
+from resources.breakfast import breakfast
+# from resources.lunch import lunch
+# from resources.dinner import dinner
+# from resources.snacks import snacks
+# from resources.smoothies import smoothies
+# from resources.desserts import desserts
+# from resources.analytics_form import analytics_form
+# from resources.workout import workout
+# from resources.chat import chat
+
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request"""
+    g.db.close()
+    return response
+
+
+CORS(breakfast, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(lunch, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(dinner, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(smoothies, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(desserts, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(snacks, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(analytics_form, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(workout, origins=['http://localhost:3000'], supports_credentials=True)
+# CORS(chat, origins=['http://localhost:3000'], supports_credentials=True)
+
+app.register_blueprint(breakfast, url_prefix='/api/v1/breakfast')
+# app.register_blueprint(lunch, url_prefix='/api/v1/lunch')
+# app.register_blueprint(dinner, url_prefix='/api/v1/dinner')
+# app.register_blueprint(snacks, url_prefix='/api/v1/smoothies')
+# app.register_blueprint(smoothies, url_prefix='/api/v1/snacks')
+# app.register_blueprint(desserts, url_prefix='/api/v1/desserts')
+# app.register_blueprint(analytics_form, url_prefix='/api/v1/form')
+# app.register_blueprint(workout, url_prefix='/api/v1/workout')
+# app.register_blueprint(chat, url_prefix='/api/v1/chat')
+
+
+
+if __name__ == '__main__':
+    print('tables connected')
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
+
+
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,124 @@
+from pymongo import MongoClient
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client= os.getenv("ATLAS_URI")
+db = client['fitfusion']
+collection = db['breakfast']
+
+class Breakfast:
+    def __init__(self, title, img, time, ingredients, description):
+        self.title = str(title)
+        self.img = str(img)
+        self.time = int(time)
+        self.ingredients = str(ingredients)
+        self.description = str(description)
+
+    def save(self):
+        document = {
+            'title': self.title,
+            'img': self.img,
+            'time': self.time,
+            'ingredients': self.ingredients,
+            'description': self.description
+        }
+        result = collection.insert_one(document)
+        return result.inserted_id
+    
+    @staticmethod
+    def get_all():
+        result = collection.find()
+        return list(result)
+
+    @staticmethod
+    def get_one(id):
+        result = collection.find_one({'_id': ObjectId(id)})
+        print(result)
+        return result
+
+    @staticmethod
+    def update(id, data):
+        result = collection.update_one({'_id': id}, {'$set': data})
+        return result.modified_count
+
+    @staticmethod
+    def delete(id):
+        result = collection.delete_one({'_id': id})
+        return result.deleted_count
+
+
+
+
+
+
+# class Lunch(Model):
+#     title = CharField()
+#     img = CharField()
+#     time = IntegerField()
+#     ingredients = IntegerField()
+#     description = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Dinner(Model):
+#     title = CharField()
+#     img = CharField()
+#     time = IntegerField()
+#     ingredients = IntegerField()
+#     description = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Desserts(Model):
+#     title = CharField()
+#     img = CharField()
+#     time = IntegerField()
+#     ingredients = IntegerField()
+#     description = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Snacks(Model):
+#     title = CharField()
+#     img = CharField()
+#     ingredients = IntegerField()
+#     description = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Smoothies(Model):
+#     title = CharField()
+#     img = CharField()
+#     time = IntegerField()
+#     ingredients = IntegerField()
+#     description = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Analytics(Model):
+#     activity = CharField()
+#     time = TimeField()
+
+#     class Meta:
+#         database = DATABASE
+
+# class Workout(Model):
+#     activity = CharField()
+#     time = TimeField()
+#     calories = IntegerField()
+#     link = CharField()
+
+#     class Meta:
+#         database = DATABASE
+
+
+def initialize():
+
+    print("TABLES Created")
