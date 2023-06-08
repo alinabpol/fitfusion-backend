89ebb142d5d4669987371aacce4206c62883b1e8
Alina232323 <alina.polianska@vergesense.com>
2023-06-07 20:09:55 -0700

    removed staicmethods from the schema

--- models/nutrition_models.py
+++ models/nutrition_models.py
@@ -37,27 +37,10 @@ class Breakfast:
         result = breakfast_collection.insert_one(document)
         return result.inserted_id
     
-    # @staticmethod
-    # def get_all():
-    #     result = collection.find()
-    #     print(result)
-    #     return list(result)
-
-    @staticmethod
-    def get_one(id):
-        result = collection.find_one({'_id': ObjectId(id)})
-        print(result)
-        return result
-
-    @staticmethod
-    def update(id, data):
-        result = collection.update_one({'_id': ObjectId(id)}, {'$set': data})
-        print(result.modified_count)
-        return result.modified_count
 
     @staticmethod
     def delete(id):
-        result = collection.delete_one({'_id': ObjectId(id)})
+        result = breakfast_collection.delete_one({'_id': ObjectId(id)})
         print(result.deleted_count)
         return result.deleted_count
 
